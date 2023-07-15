766ce3ab985b4f66286660e878661809d5aec6f1
Nahuel <nahuel.profesor@gmail.com>
2023-07-15 19:07:54 -0300

    persistencia de los objetos agregada

--- orm/main.py
+++ orm/main.py
@@ -26,6 +26,16 @@ if __name__ == '__main__':
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     
+    user1 = User(username='user1', email='user1@example.com')
+    user2 = User(username='user2', email='user2@example.com')
+    user3 = User(username='user3', email='user3@example.com')
     
+    # añadiendo los cambios al stack 
+    session.add(user1)
+    session.add(user2)
+    session.add(user3)
+    
+    # ejecutando y persistiendo los cambios
+    session.commit()
     
     
