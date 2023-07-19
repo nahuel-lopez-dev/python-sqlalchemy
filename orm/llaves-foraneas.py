586f55a667d4c09f96ab10fee549971b13274b38
Nahuel <nahuel.profesor@gmail.com>
2023-07-19 01:18:43 -0300

    relaciones entre objetos agregadas

--- orm/llaves-foraneas.py
+++ orm/llaves-foraneas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 engine = create_engine('postgresql://postgres:@localhost/pythonpg')
@@ -14,6 +14,7 @@ class User(Base):
     username = Column(String(50), nullable=False, unique=True)
     email = Column(String(50), nullable=False, unique=True)
     created_at = Column(DateTime(), default=datetime.now())
+    courses = relationship('Course', backref='user') # creando la relación
     
     def __str__(self):
         return self.username
@@ -47,6 +48,16 @@ if __name__ == '__main__':
     session.commit()
 
     course1 = Course(title='Curso profesional de Base de Datos', user_id=user1.id)
+    course2 = Course(title='Curso profesional de Python', user_id=user1.id)
+    course3 = Course(title='Curso profesional de JavaScript', user_id=user1.id)
+    
     session.add(course1)
+    session.add(course2)
+    session.add(course3)
+    
     session.commit()
     
+    # for course in user1.courses:
+    #     print(course)
+    
+    print(course1.user)
